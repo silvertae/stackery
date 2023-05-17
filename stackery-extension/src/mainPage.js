71f11c4bd4a734e830571ae9b7b9648d27259ba8
silvertae <massenger7@gmail.com>
2023-05-18 00:10:02 +0900

    feat: 카테고리별로 다른 배경 색상을 적용하는 기능 추가
    미리 지정된 색상 풀에서 카테고리가 만들어지는 순서대로
    카테고리 각 항목들의 배경색상을 적용하는 기능을 추가하였다.

--- stackery-extension/src/mainPage.js
+++ stackery-extension/src/mainPage.js
@@ -4,8 +4,10 @@ export function initMain() {
 }
 
 const mainStore = {
-  categoryList: ['CS16', 'JS']
+  categoryList: ['CS16', 'JS', 'CSS']
 };
+
+const categoryColors = ['#edede9', '#d6ccc2', '#f5ebe0', '#e3d5ca', '3d5bdaf'];
 
 function render() {
   const mainElement = document.querySelector('#main');
@@ -16,7 +18,7 @@ function render() {
     </header>
     <section class="main__body">
       ${mainStore.categoryList
-        .map((category) => makeCategory(category))
+        .map((category, idx) => makeCategory(category, idx))
         .join('')}
     </section>
   `;
@@ -24,17 +26,18 @@ function render() {
   mainElement.innerHTML = mainTemplate;
 }
 
-function makeCategory(category) {
+function makeCategory(category, idx) {
+  const categoryColor = categoryColors[idx % 5];
   const categoryTemplate = `
-    <div class="scrap__category mx-auto w-75 mt-3 border">
+    <div class="scrap__category mx-auto w-75 mt-3">
       <header class="category__header d-flex justify-content-between">
         <h3 class="category__title">${category}</h3>
         <button type="button" class="btn--close border rounded-pill invisible">접기</button>
       </header>
       <div class="category__container w-100 d-flex flex-column align-items-center">
-        <div class="category__item border rounded bg-info">브라우저는 어떻게 동작하는가?</div>
-        <div class="category__item border rounded bg-info">NAVER D2</div>
-        <div class="category__item border rounded bg-info">네트워크 동작 원리</div>
+        <div class="category__item border rounded" style="background-color: ${categoryColor}">브라우저는 어떻게 동작하는가?</div>
+        <div class="category__item border rounded" style="background-color: ${categoryColor}">NAVER D2</div>
+        <div class="category__item border rounded" style="background-color: ${categoryColor}">네트워크 동작 원리</div>
       </div>
     </div>
   `;
