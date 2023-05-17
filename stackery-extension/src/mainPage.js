ccc5e0b408df1b8fcb3b60c5c74cd5456e5b6145
silvertae <massenger7@gmail.com>
2023-05-17 23:55:13 +0900

    feat: 카테고리 클릭시 펼쳐지고 접히는 애니메이션 효과 추가
    - setEvent로 렌더링 된 후에 필요한 부분에 이벤트를 등록
    - 각 카테고리 클릭시 내부 항목들이 펼쳐지도록 하는 spreadItems 함수 추가
    - 접기 버튼 클릭시 내부 항목들이 접히도록 하는 stackItems 함수 추가

--- stackery-extension/src/mainPage.js
+++ stackery-extension/src/mainPage.js
@@ -1,6 +1,6 @@
 export function initMain() {
   render();
-  // setEvent();
+  setEvent();
 }
 
 const mainStore = {
@@ -39,4 +39,55 @@ function makeCategory(category) {
     </div>
   `;
   return categoryTemplate;
+}
+
+function setEvent() {
+  const categoryContainers = document.querySelectorAll('.category__container');
+  const closeBtn = document.querySelectorAll('.btn--close');
+
+  categoryContainers.forEach((container) => {
+    container.addEventListener('click', spreadItems);
+  });
+  closeBtn.forEach((btn) => {
+    btn.addEventListener('click', stackItems);
+  });
+}
+
+function spreadItems(e) {
+  const container = e.target.closest('.category__container');
+  const items = container.querySelectorAll('.category__item');
+  const closeBtn = e.target
+    .closest('.scrap__category')
+    .querySelector('.btn--close');
+
+  let translatePx = 0;
+
+  items.forEach((item) => {
+    closeBtn.classList.remove('invisible');
+    item.classList.remove('invisible');
+    item.style.width = '266px';
+    item.style.transform = `translateY(${translatePx}px)`;
+    translatePx += 10;
+  });
+}
+
+function stackItems(e) {
+  const category = e.target.closest('.scrap__category');
+  const container = category.querySelector('.category__container');
+  const items = category.querySelectorAll('.category__item');
+  const closeBtn = e.target;
+
+  let width = 266;
+  let translatePx = 0;
+
+  items.forEach((item, idx) => {
+    closeBtn.classList.add('invisible');
+    if (idx >= 3) {
+      item.classList.add('invisible');
+    }
+    item.style.width = `${width}px`;
+    item.style.transform = `translateY(${translatePx}px)`;
+    width -= 10;
+    translatePx -= 40;
+  });
 }
