8e3acc15a96d3efc756390af582da2653d58f724
hjsong <song6240@naver.com>
2023-05-17 23:44:13 +0900

    feat: 더보기 버튼 클릭시 선택모드로 진입 동작
    - 더보기 버튼 클릭 이벤트에 선택모드 상태 변경 함수를 등록
    - 상태 변경 후 재렌더링
    - 메인페이지 헤더 렌더링 시 선택모드 상태에 따라 다른 템플릿을 구성
    - 취소, 완료 버튼에 선택모드 종료하는 함수를 클릭 이벤트 핸들러로 등록

--- stackery-extension/src/header.js
+++
@@ -1,4 +0,0 @@
-export const getHeaderTemplate =
-  () => `<header class="main__header d-flex justify-content-end">
-  <button type="button" class="btn"><i class="bi bi-three-dots"></i></button>
-</header>`;
--- stackery-extension/src/main.js
+++ stackery-extension/src/main.js
@@ -1,5 +1,94 @@
-import { getContentBodyTemplate } from "./contentBody.js";
-import { getHeaderTemplate } from "./header.js";
-
 const mainElement = document.getElementById('main');
-mainElement.innerHTML = getHeaderTemplate() + getContentBodyTemplate();
+const state = {
+  selectMode: false,
+  scraps: [
+    {
+      title: '자바스크립트',
+      items: [
+        { title: '브라우저 렌더링1', url: '~' },
+        { title: '브라우저 렌더링2', url: '~' },
+        { title: '브라우저 렌더링3', url: '~' }
+      ]
+    },
+    {
+      title: '자바스크립트',
+      items: [
+        { title: '브라우저 렌더링1', url: '~' },
+        { title: '브라우저 렌더링2', url: '~' },
+        { title: '브라우저 렌더링3', url: '~' }
+      ]
+    }
+  ]
+};
+
+const getHeaderTemplate = (state) => {
+  const headerTemplate = state.selectMode
+    ? `<header class="main__header d-flex justify-content-between">
+    <button type="button" class="main__cancle-button btn"><i class="bi bi-x-lg"></i></button>
+    <button type="button" class="main__complete-button btn"><i class="bi bi-check-lg"></i></button>
+  </header>`
+    : `<header class="main__header d-flex justify-content-end">
+      <button type="button" class="main__more-button btn"><i class="bi bi-three-dots"></i></button>
+    </header>`;
+  return headerTemplate;
+};
+
+const getContentBodyTemplate = (state) => `<section class="main__body">
+  ${createCategoriesTemplate(state.scraps)}
+</section>`;
+
+const createCategoriesTemplate = (categories) =>
+  categories
+    .map(
+      (category) => `
+  <div class="scrap__category">
+    <header class="category__header d-flex justify-content-between">
+    <h3 class="category__title">${category.title}</h3>
+      <button type="button" class="btn">접기</button>
+    </header>
+    <div class="category__container">
+      ${createItemsTemplate(category.items)}
+    </div>
+  </div>
+`
+    )
+    .join('');
+
+const createItemsTemplate = (items) =>
+  items
+    .map((item) => `<div class="category__item">${item.title}</div>`)
+    .join('');
+
+const setEvent = () => {
+  const moreButton = mainElement.querySelector('.main__more-button');
+  if (moreButton) {
+    moreButton.addEventListener('click', turnOnSelectMode);
+  }
+
+  const cancleButton = mainElement.querySelector('.main__cancle-button');
+  if (cancleButton) {
+    cancleButton.addEventListener('click', turnOffSelectMode);
+  }
+  const completeButton = mainElement.querySelector('.main__complete-button');
+  if (completeButton) {
+    completeButton.addEventListener('click', turnOffSelectMode);
+  }
+};
+
+const render = () => {
+  mainElement.innerHTML =
+    getHeaderTemplate(state) + getContentBodyTemplate(state);
+  setEvent();
+};
+
+const turnOnSelectMode = () => {
+  state.selectMode = true;
+  render();
+};
+
+const turnOffSelectMode = () => {
+  state.selectMode = false;
+  render();
+};
+
+render();
