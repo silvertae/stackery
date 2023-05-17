4ee4ba3298ea27ed3f4b01e475df410a48617b09
silvertae <massenger7@gmail.com>
2023-05-18 02:00:19 +0900

    feat: 카테고리의 항목 클릭시 해당 url을 새 탭에서 여는 기능 추가
    - 접힌 상태에서는 각 항목들은 클릭되지 않음 (css pointer-events 속성 사용)
    - 펼쳐진 상태에서 각 항목을 클릭하면 dataset에 있는 url 링크를 브라우저의
    새 탭에서 연다. (chrome.tabs API 사용)
    - chrome API 사용을 위해 manifest.json에 permission 추가
    - 펼쳐진 상태와 접힌 상태를 spread라는 클래스로 구분해서 category__container의
    height를 조절하는 방법 시도중

--- stackery-extension/src/mainPage.js
+++ stackery-extension/src/mainPage.js
@@ -35,9 +35,9 @@ function makeCategory(category, idx) {
         <button type="button" class="btn--close border rounded-pill invisible">접기</button>
       </header>
       <div class="category__container w-100 d-flex flex-column align-items-center">
-        <div class="category__item border rounded" style="background-color: ${categoryColor}">브라우저는 어떻게 동작하는가?</div>
-        <div class="category__item border rounded" style="background-color: ${categoryColor}">NAVER D2</div>
-        <div class="category__item border rounded" style="background-color: ${categoryColor}">네트워크 동작 원리</div>
+        <div class="category__item border rounded" style="background-color: ${categoryColor}" data-url="https://d2.naver.com/helloworld/59361">브라우저는 어떻게 동작하는가?</div>
+        <div class="category__item border rounded" style="background-color: ${categoryColor}" data-url="https://d2.naver.com/helloworld">NAVER D2</div>
+        <div class="category__item border rounded" style="background-color: ${categoryColor}" data-url="https://developer.mozilla.org/ko/docs/Learn/Common_questions/Web_mechanics/How_does_the_Internet_work">네트워크 동작 원리</div>
       </div>
     </div>
   `;
@@ -47,12 +47,19 @@ function makeCategory(category, idx) {
 function setEvent() {
   const categoryContainers = document.querySelectorAll('.category__container');
   const closeBtn = document.querySelectorAll('.btn--close');
+  const items = document.querySelectorAll('.category__item');
 
   categoryContainers.forEach((container) => {
     container.addEventListener('click', spreadItems);
   });
   closeBtn.forEach((btn) => {
     btn.addEventListener('click', stackItems);
+  });
+  items.forEach((item) => {
+    item.addEventListener('click', () => {
+      console.log(item.className);
+      chrome.tabs.create({ url: item.dataset.url });
+    });
   });
 }
 
@@ -67,8 +74,11 @@ function spreadItems(e) {
 
   items.forEach((item) => {
     closeBtn.classList.remove('invisible');
+    container.classList.add('spread');
     item.classList.remove('invisible');
+    item.classList.add('spread');
     item.style.width = '266px';
+    item.style.height = '50px';
     item.style.transform = `translateY(${translatePx}px)`;
     translatePx += 10;
   });
@@ -83,11 +93,13 @@ function stackItems(e) {
   let width = 266;
   let translatePx = 0;
 
+  container.classList.remove('spread');
   items.forEach((item, idx) => {
     closeBtn.classList.add('invisible');
     if (idx >= 3) {
       item.classList.add('invisible');
     }
+    item.classList.remove('spread');
     item.style.width = `${width}px`;
     item.style.transform = `translateY(${translatePx}px)`;
     width -= 10;
