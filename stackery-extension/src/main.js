790cfbc01159cab4c83124baf13e0b584339145f
hjsong <song6240@naver.com>
2023-05-18 12:03:18 +0900

    feat: 더보기 버튼 드롭다운 선택으로 공유,삭제,열기 실행
    - 더보기 클릭시 드롭다운 열기
    - 공유, 삭제, 열기 클릭시 선택모드 목적을 share, remove, open으로 변경
    - 확인 클릭 시 선택모드 목적에 따라 처리
    - 선택모드 종료 시 선택모드 목적 속성 제거, 체크된 항목 초기화

--- stackery-extension/src/main.js
+++ stackery-extension/src/main.js
@@ -5,17 +5,47 @@ const state = {
     {
       title: '카테고리1',
       items: [
-        { title: '스크랩 제목1', url: '~', checked: false },
-        { title: '스크랩 제목2', url: '~', checked: false },
-        { title: '스크랩 제목3', url: '~', checked: false }
+        {
+          id: 1,
+          title: '스크랩 제목1',
+          url: 'https://getbootstrap.com/docs/5.3/getting-started/introduction/',
+          checked: false
+        },
+        {
+          id: 2,
+          title: '스크랩 제목2',
+          url: 'https://developer.chrome.com/docs/extensions/reference/storage/',
+          checked: false
+        },
+        {
+          id: 3,
+          title: '스크랩 제목3',
+          url: 'https://www.youtube.com/watch?v=sMivc3RIrFs',
+          checked: false
+        }
       ]
     },
     {
       title: '카테고리2',
       items: [
-        { title: '스크랩 제목1', url: '~', checked: false },
-        { title: '스크랩 제목2', url: '~', checked: false },
-        { title: '스크랩 제목3', url: '~', checked: false }
+        {
+          id: 4,
+          title: '스크랩 제목1',
+          url: 'https://developer.mozilla.org/ko/docs/Web/API/Event/bubbles',
+          checked: false
+        },
+        {
+          id: 5,
+          title: '스크랩 제목2',
+          url: 'https://www.naver.com/',
+          checked: false
+        },
+        {
+          id: 6,
+          title: '스크랩 제목3',
+          url: 'https://www.google.com/',
+          checked: false
+        }
       ]
     }
   ]
@@ -28,7 +58,14 @@ const getHeaderTemplate = (state) => {
     <button type="button" class="main__complete-button btn"><i class="bi bi-check-lg"></i></button>
   </header>`
     : `<header class="main__header d-flex justify-content-end">
-      <button type="button" class="main__more-button btn"><i class="bi bi-three-dots"></i></button>
+      <div class="dropdown">
+        <button type="button" class="main__more-button btn dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false"><i class="bi bi-three-dots"></i></button>
+        <ul class="dropdown-menu dropdown-menu-dark">
+          <li class="dropdown-item" data-purpose="share"><a class="dropdown-item" href="#">공유하기</a></li>
+          <li class="dropdown-item" data-purpose="remove"><a class="dropdown-item" href="#">삭제하기</a></li>
+          <li class="dropdown-item" data-purpose="open"><a class="dropdown-item" href="#">모두 열기</a></li>
+        </ul>
+      </div>
     </header>`;
   return headerTemplate;
 };
@@ -58,11 +95,13 @@ const createItemsTemplate = (items, isSelectMode) =>
   items
     .map(
       (item, index) => `
-    <div class="item__wrapper d-flex">
-      <div class="item__title">${item.title}</div>
-      ${getSelectBoxForItem(item.checked, isSelectMode, index)}
-    </div>
-    `
+        <div class="item__wrapper d-flex">
+          <div class="item__title">
+            <p>${item.title}</p>
+          </div>
+          ${getSelectBoxForItem(item.checked, isSelectMode, index)}
+        </div>
+        `
     )
     .join('');
 
@@ -74,9 +113,15 @@ const getSelectBoxForItem = (checked, isSelectMode, index) =>
     : '';
 
 const setEvent = () => {
-  const moreButton = mainElement.querySelector('.main__more-button');
-  if (moreButton) {
-    moreButton.addEventListener('click', turnOnSelectMode);
+  const dropdownItems = mainElement.querySelectorAll('.dropdown-item');
+  if (dropdownItems) {
+    dropdownItems.forEach((dropdownItem) => {
+      dropdownItem.addEventListener('click', (event) => {
+        const item = event.currentTarget;
+        setSelectionPurpose(item.dataset.purpose);
+        turnOnSelectMode();
+      });
+    });
   }
   const cancleButton = mainElement.querySelector('.main__cancle-button');
   if (cancleButton) {
@@ -95,8 +140,8 @@ const setEvent = () => {
           event.stopPropagation();
           return;
         }
-        const categoryIndex = category.dataset.categoryIndex;
-        const itemIndex = checkbox.dataset.itemIndex;
+        const { categoryIndex } = category.dataset;
+        const { itemIndex } = checkbox.dataset;
         changeCheckedForItem(categoryIndex, itemIndex);
       });
     });
@@ -116,21 +161,60 @@ const turnOnSelectMode = () => {
 
 const turnOffSelectMode = () => {
   state.selectMode = false;
+  resetSelectionStates();
   render();
 };
 
+const setSelectionPurpose = (purpose) => {
+  state.selectionPurpose = purpose;
+};
+
 const completeSelectMode = () => {
-  const selectedItems = [];
-  state.scraps.forEach((scrap) => {
-    selectedItems.push(...scrap.items.filter((item) => item.checked));
-  })
+  const selectedItems = state.scraps
+    .map((scrap) => scrap.items.filter((item) => item.checked))
+    .reduce((p, n) => [...p, ...n], []);
+  handleSelectedItemsWithPurpose(selectedItems);
   turnOffSelectMode();
-}
+};
+
+const handleSelectedItemsWithPurpose = (selectedItems) => {
+  switch (state.selectionPurpose) {
+    case 'share':
+      navigator.clipboard.writeText(
+        selectedItems.map((item) => item.url).join('\n')
+      );
+      break;
+    case 'remove':
+      const itemIds = selectedItems.map((selectedItem) => selectedItem.id);
+      state.scraps = state.scraps.map((scrap) => {
+        scrap.items = scrap.items.filter((item) => itemIds.includes(item.id));
+        return scrap;
+      });
+      break;
+    case 'open':
+      selectedItems.forEach((selectedItem) => {
+        chrome.tabs.create({ url: selectedItem.url, active: false });
+      });
+      break;
+    default:
+      break;
+  }
+};
+
+const resetSelectionStates = () => {
+  delete state.selectionPurpose;
+  state.scraps = state.scraps.map((scrap) => {
+    scrap.items.forEach((item) => {
+      item.checked = false;
+    });
+    return scrap;
+  });
+};
 
 const changeCheckedForItem = (categoryIndex, itemIndex) => {
   const item = state.scraps[categoryIndex].items[itemIndex];
   state.scraps[categoryIndex].items[itemIndex].checked = !item.checked;
   render();
-}
+};
 
 render();
